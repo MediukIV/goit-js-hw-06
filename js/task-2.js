80ab73ee6ec96fc75dc1f09787806c7e6e2af841
MediukIV <143748068+MediukIV@users.noreply.github.com>
2023-12-09 21:04:20 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,22 @@
 class Storage {
+  #items
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    this.items.splice(this.items.indexOf(itemToRemove), 1);
+    let index = this.#items.indexOf(itemToRemove);
+    if (index > -1) {
+      this.#items.splice(index, 1);
+    }
   }
 }
 
@@ -22,4 +26,3 @@ console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
